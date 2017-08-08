0d87e7bff8e10d3ab319c6fb3c6be26a99187b56
Ndjawe-Akambiet <e.ndjawe@gmail.com>
2017-08-08 17:28:03 +0200

    refactor(*): indent

--- parsing-json.go
+++ parsing-json.go
@@ -14,22 +14,22 @@ type Person struct {
 
 func main() {
   	keysBody := []byte(`[{"name": "sam", "age": 27, "details": {"salary":10000} }, {"name": "john", "age": 31, "details": {"salary":12000} }]`)
-	  var data []Person
+	var data []Person
  
-   /*json.Unmarshal is used when the input is []byte*/
-	 /*stores the decoded result in map named data*/
-	 err := json.Unmarshal(keysBody, &data)
+	/*json.Unmarshal is used when the input is []byte*/
+	/*stores the decoded result in map named data*/
+	err := json.Unmarshal(keysBody, &data)
 
-   if err != nil {
+	if err != nil {
 		panic(err)
 	}
   
-  fmt.Printf("==data==%#v \n\n", data)
+	fmt.Printf("==data==%#v \n\n", data)
 	fmt.Println("==name== ", data[0].Name)
 	fmt.Println("==name== ", data[1].Name)
 
-  /*Type assertions, check type of data["details"]*/
-  details, ok := data[0].Details.(map[string]interface{})
+	/*Type assertions, check type of data["details"]*/
+	details, ok := data[0].Details.(map[string]interface{})
 	if ok {
 		fmt.Println("==salary== ", details["salary"])
 	}
